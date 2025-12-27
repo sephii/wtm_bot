daf6aea560f158c48253b2ad9935b64dc5dbdaf1
Sylvain Fankhauser <sephi@nibelheim.ch>
2025-12-27 09:45:59 +0100

    Upgrade nixpkgs to 25.11

--- flake.nix
+++ flake.nix
@@ -1,16 +1,23 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
-  outputs = { nixpkgs, ... }:
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-25.11";
+  outputs =
+    { nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-    in rec {
+    in
+    rec {
       packages.${system}.default = pkgs.python3.pkgs.buildPythonApplication {
         pname = "whatthemovie";
         version = "1.0";
+        pyproject = true;
+        build-system = [ pkgs.python3.pkgs.setuptools ];
         src = ./.;
         doCheck = false;
-        nativeBuildInputs = with pkgs.python3.pkgs; [ setuptools pip ];
+        nativeBuildInputs = with pkgs.python3.pkgs; [
+          setuptools
+          pip
+        ];
 
         propagatedBuildInputs = with pkgs.python3.pkgs; [
           httpx
